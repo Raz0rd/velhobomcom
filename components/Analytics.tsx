0241c23a1e53a14f2714c54427eb455d63d2f174
Raz0rd <vasiduu@gmail.com>
2025-11-12 12:30:24 -0300

    Otimizar Analytics: remover duplicação e garantir tags no source

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,53 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
-
 export default function Analytics() {
   const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID || 'AW-17657798942';
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
   const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '';
 
-  useEffect(() => {
-    // Google Ads / Google Analytics
-    const gtagScript = document.createElement('script');
-    gtagScript.async = true;
-    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`;
-    document.head.appendChild(gtagScript);
-
-    const gtagConfig = document.createElement('script');
-    gtagConfig.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${GOOGLE_ADS_ID}');
-      ${GA_MEASUREMENT_ID ? `gtag('config', '${GA_MEASUREMENT_ID}');` : ''}
-    `;
-    document.head.appendChild(gtagConfig);
-
-    // Meta Pixel
-    if (META_PIXEL_ID) {
-      const metaPixelScript = document.createElement('script');
-      metaPixelScript.innerHTML = `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${META_PIXEL_ID}');
-        fbq('track', 'PageView');
-      `;
-      document.head.appendChild(metaPixelScript);
-    }
-
-    return () => {
-      // Cleanup não é necessário pois os scripts devem persistir
-    };
-  }, [GOOGLE_ADS_ID, GA_MEASUREMENT_ID, META_PIXEL_ID]);
-
-  // Renderizar scripts inline também para aparecer no source
+  // Renderizar scripts diretamente no HTML para aparecer no source
   return (
     <>
       {/* Google Ads Global Site Tag - Visível no source */}
@@ -69,22 +27,32 @@ export default function Analytics() {
 
       {/* Meta Pixel - Visível no source */}
       {META_PIXEL_ID && (
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${META_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img 
+              height="1" 
+              width="1" 
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        </>
       )}
     </>
   );
